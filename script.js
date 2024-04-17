5ee87430a7c4c79eaa545f93f2d1564a33da48bf
JGamrot <145107244+Jgamrot@users.noreply.github.com>
2024-04-17 21:37:00 +0100

    Make a working etch-a-sketch
    First round, it's working! Need to figure out how
    to clear the grid with inner.HTML.
    
    Then could make it look better and add the extra steps.

---
+++ script.js
@@ -0,0 +1,35 @@
+const gridContainer = document.getElementById("container");
+let userInput = 16;
+// I could replace the 16 by a user define number in a input box, so the user can define grid size
+
+function makeGrid() {
+    const tileSize = 960 / userInput;
+    for (let i = 0; i < userInput**2; i++){
+    const gridTile = document.createElement("div");
+    gridTile.classList.add("gridTile");
+    gridTile.style.width = `${tileSize}px`;
+    gridTile.style.height = `${tileSize}px`;
+    gridContainer.appendChild(gridTile);
+
+    gridTile.addEventListener("mouseover", () => {
+        gridTile.style.backgroundColor = 'blue'; // could use a math.random to make it a random color
+        });
+    }
+}
+
+function clearGrid() {
+    gridTile.innerHTML = "";
+}
+
+makeGrid();
+
+const promptBtn = document.getElementById("prompt-Btn");
+
+promptBtn.addEventListener("click", () => {
+    userInput = prompt("How many squares per side do you want for this new grid?");
+    console.log(userInput); 
+    clearGrid();
+    makeGrid();
+
+});
+
